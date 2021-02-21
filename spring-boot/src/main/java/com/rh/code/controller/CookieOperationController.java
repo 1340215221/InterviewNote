1d4fd1b500eb68a00753ed52278430a8f72ada92
ruanhang <1340215221@qq.com>
2021-02-22 00:44:28 +0800

    update

---
+++ spring-boot/src/main/java/com/rh/code/controller/CookieOperationController.java
@@ -0,0 +1,38 @@
+package com.rh.code.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * 操作session 测试
+ */
+@RestController
+public class SessionOperationController {
+
+    /**
+     * 设置session
+     */
+    @GetMapping("addSession")
+    public void addSession(HttpSession session) {
+        session.setAttribute("token", "123456");
+        session.setMaxInactiveInterval(300); // 单位为秒
+    }
+
+    /**
+     * 设置session失效
+     */
+    @GetMapping("updateSessionInvalid")
+    public void updateSessionInvalid(HttpSession session) {
+        session.removeAttribute("token");
+    }
+
+    /**
+     * 获得session
+     */
+    @GetMapping("hasSession")
+    public void hasSession(HttpSession session) {
+        System.out.println(session.getAttribute("token"));
+    }
+}
