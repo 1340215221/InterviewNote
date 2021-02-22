2e15e9d55aef1dc3a3d2bf36f75f7f451f2cffb9
1340215221 <1340215221@qq.com>
2021-02-22 15:16:19 +0800

    update

--- spring-boot-war/src/main/java/com/rh/code/MyApplication.java
+++ spring-boot-war/src/main/java/com/rh/code/MyApplication.java
@@ -8,13 +8,13 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class MyApplication extends SpringBootServletInitializer {
 
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(MyApplication.class);
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(MyApplication.class, args);
     }
 
 }
