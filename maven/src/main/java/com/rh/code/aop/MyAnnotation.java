2c33678023a1da9a7c8872fdf119d5fda6e46abe
1340215221 <1340215221@qq.com>
2021-02-24 17:00:51 +0800

    update

--- maven/src/main/java/com/rh/code/aop/MyAnnotation.java
+++ maven/src/main/java/com/rh/code/aop/MyAnnotation.java
@@ -1,4 +1,15 @@
 package com.rh.code.aop;
 
-public class MyAnnotation {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 自定义注解
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MyAnnotation {
+    String value() default "";
 }
--- maven/src/main/java/com/rh/code/aop/MyAspect.java
+++ maven/src/main/java/com/rh/code/aop/MyAspect.java
@@ -1,4 +1,59 @@
 package com.rh.code.aop;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+
+/**
+ * 自定义切面
+ */
+@Aspect
+@Component
 public class MyAspect {
+
+    @Pointcut("execution(* com.rh.code.aop.*Action.*(..))")
+    public void matchMethod() {}
+
+    @Pointcut("@annotation(com.rh.code.aop.MyAnnotation)")
+    public void matchAnnotation() {}
+
+    @Around("matchMethod() && matchAnnotation()")
+    public Object handleSwingAsync(ProceedingJoinPoint joinPoint) throws Throwable {
+        // 前置处理
+        Object result = joinPoint.proceed();
+        // 后置处理
+        return result;
+    }
+
+    /**
+     * 获得方法名
+     */
+    public String getMethodName(ProceedingJoinPoint joinPoint) {
+        Signature signature = joinPoint.getSignature();
+        return signature.getName();
+    }
+
+    /**
+     * 获得方法传入参数
+     */
+    public Object[] getArgs(ProceedingJoinPoint joinPoint) {
+        return joinPoint.getArgs();
+    }
+
+    /**
+     * 获得注解参数
+     */
+    public String getAnnotationParam(ProceedingJoinPoint joinPoint) {
+        Signature signature = joinPoint.getSignature();
+        MethodSignature methodSignature = (MethodSignature) signature;
+        Method method = methodSignature.getMethod();
+        MyAnnotation annotation = method.getAnnotation(MyAnnotation.class);
+        return annotation.value();
+    }
 }
