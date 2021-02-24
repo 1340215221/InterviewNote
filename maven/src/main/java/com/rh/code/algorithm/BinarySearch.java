746d04180ac2fe04c5c0c8d6578055e25da28451
1340215221 <1340215221@qq.com>
2021-02-24 12:01:07 +0800

    update

--- maven/src/main/java/com/rh/code/algorithm/BinarySearch.java
+++ maven/src/main/java/com/rh/code/algorithm/BinarySearch.java
@@ -1,4 +1,25 @@
 package com.rh.code.algorithm;
 
+/**
+ * 二分查找(折半查找)
+ */
 public class BinarySearch {
+
+    public int search(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        int mid;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+            if (target < arr[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
 }
